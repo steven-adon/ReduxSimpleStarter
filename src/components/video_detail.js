c67e911929b6de88278894a7633d41a158b0e403
gengyudong <gengyd@ivymobi.com>
2018-03-11 20:09:41 +0800

    detail components

---
+++ src/components/video_detail.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const VideoDetail = ({video}) => {
+  const videoId = video.id.videoId;
+  const url = `http://www.youtube.com/embed/${videoId}`;
+
+  return (
+    <div className="video-detail col-md-8">
+      <div className="embed-responsive embed-responsive-16by9">
+        <iframe className="emebd-responsive-item" src={url}></iframe>
+        <div className="details">
+          <div>{video.snippet.title}</div>
+          <div>{video.snippet.decription}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default VideoDetail;
