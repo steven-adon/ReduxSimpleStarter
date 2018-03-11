46de59d6a1ef72d1a1087f6022db27e2b5f48d10
gengyudong <gengyd@ivymobi.com>
2018-03-11 18:15:27 +0800

    Contolled Componnets

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,14 +4,16 @@ class Search_Bar extends Component {
 
   constructor (props) {
     super(props);
-    this.state = {term: ''};
+    this.state = {term: 'Starting Value'};
   }
 
 
   render() {
     return (
       <div>
-        <input onChange={event => this.setState({term: event.target.value})}/>
+        <input 
+          value={this.state.term}
+          onChange={event => this.setState({term: event.target.value})}/>
         Value of the input: {
           this.state.term
         }
